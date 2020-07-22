e3cbb1d36aacfe6d8e6091c9a6adc01bb2d13ccb
Avaaren <avaaren@mail.ru>
2020-07-22 17:10:08 +0300

    Completed task 1.10

---
+++ Chapter_1/1.10/index.php
@@ -0,0 +1,30 @@
+<?php
+
+function isPalindrome($number) {
+    $tmp = $number;
+    $reversedNumber = 0;
+    # If number is palindrome -> number == reversed number
+    # That, reverse our tmp number and compare with original 
+    while ($tmp > 1) {
+        $lastDigit = $tmp % 10;
+        $reversedNumber = $reversedNumber * 10 + $lastDigit;
+        $tmp /= 10;
+    }
+    if ($reversedNumber != $number) {
+        return false;
+    }
+    return true;
+}
+
+function findAllSquarePalindromes($number) {
+    # Find all palindromes in range 0 - number
+    for ($i = 0; $i < $number; $i++) {
+        if ( isPalindrome($i) ) {
+            if ( isPalindrome($i * $i) ) {
+                echo "$i is pal and " . $i* $i. " is pal <br>";
+            }
+        }
+    }
+}
+
+findAllSquarePalindromes(1000);
