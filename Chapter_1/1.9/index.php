771e47293cee8be0d373472d79900b173bb67192
Avaaren <avaaren@mail.ru>
2020-07-22 16:29:44 +0300

    Completed task 1.9

---
+++ Chapter_1/1.9/index.php
@@ -0,0 +1,19 @@
+<?php
+
+function isAuthomorphic($number) {
+    $numberSquare = $number * $number;
+    echo "$number is ";
+    while ($number > 1 ) {
+        if ( ($number % 10) != ($numberSquare % 10) ) {
+            echo "not authomorphic <br>";
+            return false;
+        }
+        $number /= 10;
+        $numberSquare /= 10;
+    }
+    echo "is authomorphic <br>";
+}
+
+isAuthomorphic(25);
+isAuthomorphic(43); 
+isAuthomorphic(6);
