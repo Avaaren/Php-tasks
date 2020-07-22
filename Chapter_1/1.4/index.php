e9173365c281a738dca3f2591e5127a40c4f64d6
Avaaren <avaaren@mail.ru>
2020-07-22 14:46:58 +0300

    Completed task 1.4

---
+++ Chapter_1/1.4/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+function isIncreasingSequance($number) {
+    while ( $number / 10 > 1 ) {
+        if (!( ($number / 10 % 10) > ($number % 10) )) {
+            return false;
+        }
+        $number /= 10;
+    }
+    return true;
+}
+
+function isDecreasingSequance($number) {
+    while ( $number / 10 > 1 ) {
+        if (!( ($number / 10 % 10) < ($number % 10) )) {
+            return false;
+        }
+        $number /= 10;
+    }
+    return true;
+}
+
+for ($i = 1000; $i < 10000; $i++) {
+    if ($i % 2 == 0) {
+        if (isIncreasingSequance($i) or isDecreasingSequance($i)){
+            echo "$i, ";
+        }
+    }
+}
