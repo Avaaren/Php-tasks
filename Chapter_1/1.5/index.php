dec82e8383fa909b5ae5bfd82bfcf1726ef55a84
Avaaren <avaaren@mail.ru>
2020-07-22 15:02:13 +0300

    Completed task 1.5

---
+++ Chapter_1/1.5/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+function reverseDigit($number) {
+    echo "$number -> ";
+    $reversedDigit = 0;
+    while ($number > 1) {
+        # Получаем последнее число и циклом увеличиваем его разрядность добавляя след идущие
+        $lastDigit = $number % 10;
+        $number /= 10;
+        # 423 = 0*10+3 -> 3*10+2 -> 32*10+4
+        $reversedDigit *= 10;
+        $reversedDigit += $lastDigit;
+    }
+    echo "$reversedDigit <br>";
+}
+
+reverseDigit(435839);
+reverseDigit(123456789);
