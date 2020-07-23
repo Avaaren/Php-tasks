41f1a093f4602c87863ab712998011fcc232ee0c
Avaaren <avaaren@mail.ru>
2020-07-23 14:04:40 +0300

    Merged two functions

--- Chapter_1/1.4/index.php
+++ Chapter_1/1.4/index.php
@@ -1,29 +1,39 @@
 <?php 
 
-function isIncreasingSequance($number) {
-    while ( $number / 10 >= 1 ) {
-        if (!( ($number / 10 % 10) > ($number % 10) )) {
-            return false;
+function isSequance($number) {
+    # Сравниваем два последних числа. Если последнее больше предпоследнего
+    # То это либо возрастающая последовательность либо никакая. И наоборот.
+    if ( ($number / 10 % 10) > ($number % 10) ) {
+        // Decreasing
+        while ( $number / 10 >= 1 ) {
+            if (!( ($number / 10 % 10) > ($number % 10) )) {
+                return false;
+            }
+            $number /= 10;
         }
+    }  
+    if ( ($number / 10 % 10) < ($number % 10) ) {
+        // Increasing
+        while ( $number / 10 >= 1 ) {
+            if (!( ($number / 10 % 10) < ($number % 10) )) {
+                return false;
+            }
         $number /= 10;
-    }
-    return true;
-}
-
-function isDecreasingSequance($number) {
-    while ( $number / 10 >= 1 ) {
-        if (!( ($number / 10 % 10) < ($number % 10) )) {
-            return false;
         }
-        $number /= 10;
     }
-    return true;
+    # Если два соседних числа равны то число не последовательность
+    if ( ($number / 10 % 10) == ($number % 10) ) {
+        return false;
+    }
+    # Если число все таки последовательность то возвращаем true
+    return true; 
 }
 
 for ($i = 1000; $i < 10000; $i++) {
     if ($i % 2 == 0) {
-        if (isIncreasingSequance($i) or isDecreasingSequance($i)){
+        if (isSequance($i)){
             echo "$i, ";
         }
     }
 }
+
