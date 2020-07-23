91f35a1e66f1394a9b1942590f57d710e2fd4e89
Avaaren <avaaren@mail.ru>
2020-07-23 12:36:52 +0300

    Reworked for task conditions

--- Chapter_1/1.6/index.php
+++ Chapter_1/1.6/index.php
@@ -1,10 +1,22 @@
 <?php 
 
 function checkNumberForCondition($number) {
-    while ( $number >= 1) {
-        $lastDigit = $number % 10;
+    $tmp = $number;
+    while ( $tmp >= 1) {
+        $lastDigit = $tmp % 10;
         if ($lastDigit != 0 && $lastDigit != 2 && $lastDigit != 3 && $lastDigit != 7){
             return false;
+        }
+        $tmp /= 10;
+    }
+    while ($number >= 1) {
+        $lastDigit = $number % 10;
+        $innerNumber = $number / 10;
+        while ($innerNumber >= 1) {
+            if ($lastDigit == ($innerNumber % 10)){
+                return false;
+            }
+            $innerNumber /= 10;
         }
         $number /= 10;
     }
