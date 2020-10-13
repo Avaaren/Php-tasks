bff9fe8a4d36bc5e43446e7108d4fccf2a17d65a
Avaaren <avaaren@mail.ru>
2020-10-13 10:23:45 +0300

    Completed task 2.37

---
+++ Chapter_2/2.37/index.php
@@ -0,0 +1,61 @@
+<?php
+
+function findPositiveIndex($array)
+{
+    for ($i = 1; $i < sizeof($array); $i++)
+    {
+        if ($array[$i] > 0)
+        {
+            return $i;
+        }
+    }
+}
+
+function sortArray($array, $sorted=false)
+{
+    $swapped = true;
+    while($swapped)
+    {
+        if (!$sorted)
+        {
+            $swapped = false;
+            for ($i = 1; $i < sizeof($array) ; $i++)
+            {
+                if ( $array[$i - 1] > 0 && $array[$i] == 0 )
+                {
+                    $tmp = $array[$i - 1];
+                    $array[$i - 1] = $array[$i];
+                    $array[$i] = $tmp;
+                    $swapped = true;
+                } 
+            }
+        }
+        else
+        {
+            $swapped = false;
+            for ($i = findPositiveIndex($array) + 1; $i < sizeof($array) ; $i++)
+            {
+                if ( $array[$i - 1] > $array[$i] )
+                {
+                    $tmp = $array[$i - 1];
+                    $array[$i - 1] = $array[$i];
+                    $array[$i] = $tmp;
+                    $swapped = true;
+                } 
+            }
+        } 
+    }
+    
+    if (!$sorted)
+    {
+        $array = sortArray($array, $sorted=true);
+        print_r($array);
+    }
+    else 
+    {
+        return $array;
+    }
+}
+
+$array = array(1, 5, 0, 8, 0, 2);
+sortArray($array, false);
